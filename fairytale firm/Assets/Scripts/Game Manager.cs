98d14f456e4177e998bda9edd7a6e694d2935e61
stayup24h <sani0501we@gmail.com>
2024-04-01 01:02:22 +0900

    docs: write readme.md ad a plan

--- fairytale firm/Assets/Scripts/Game Manager.cs
+++ fairytale firm/Assets/Scripts/Game Manager.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class GameManager : Singleton<GameManager>
 {
-    public static int turn_Count, numberOfMoney, numberOfWood, NumberOfBook; //재화 변수 선언
+    public int numberOfTurn, numberOfMoney, numberOfWood, NumberOfBook; //재화 변수 선언
 
     public void NextTurn() //다음턴 함수
     {
-        turn_Count += 1;
+        numberOfTurn += 1;
     }
 }
