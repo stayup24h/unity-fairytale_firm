96f1a9e7ec7bfa28b8f54f3ea29335e8d3daff31
stayup24h <sani0501we@gmail.com>
2024-04-05 20:16:37 +0900

    update README.md

--- fairytale firm/Assets/Scripts/Game Manager.cs
+++ fairytale firm/Assets/Scripts/Game Manager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    public int numberOfTurn, numberOfMoney, numberOfWood, NumberOfBook; //재화 변수 선언
+    public int numberOfMoney, numberOfWood, NumberOfBook; //재화 변수 선언
+    public int countOfTurn; 
+
+    public int productOfGetWood = 3, needOfGetWood = 100;
 
     public void NextTurn() //다음턴 함수
     {
         numberOfTurn += 1;
+    }
+
+    public void GetWood()
+    {
+        numberOfMoney -= needOfGetWood;
+        numberOfWood += productOfGetWood;
     }
 }
