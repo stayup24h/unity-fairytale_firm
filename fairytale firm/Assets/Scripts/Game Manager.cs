af8d435c9d3921df229e0ea336a1b05df3d2f590
stayup24h <sani0501we@gmail.com>
2024-04-08 01:27:52 +0900

    feat: add Data Save and Load

--- fairytale firm/Assets/Scripts/Game Manager.cs
+++ fairytale firm/Assets/Scripts/Game Manager.cs
@@ -1,22 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
-    public int numberOfMoney, numberOfWood, NumberOfBook; //재화 변수 선언
-    public int countOfTurn; 
-
+    private string hasSavefile = null; // 데이터 파일 존재 여부
+    private int isClearedNE, isClearedTE, isClearedBE, isClearedHE;
+    public GoodsVariable goodsVariable; // 재화 변수
+    public NumberVariable numberVariable; // 횟수 변수
     public int productOfGetWood = 3, needOfGetWood = 100;
+
+    public void Save()
+    {
+        hasSavefile = PlayerPrefs.GetString("hasSavefile");
+        if (hasSavefile != null) // 데이터 파일이 있으면 변경된 값만 저장
+        {
+            PlayerPrefs.Save();
+        }
+        else // 데이터 파일이 없으면 생성
+        {
+            // 데이터 파일 존재여부 업데이트
+            PlayerPrefs.SetString("hasSavefile", "true");
+            // 재화 변수 데이터 파일
+            PlayerPrefs.SetInt("amountOfMoney", goodsVariable.amountOfMoney);
+            PlayerPrefs.SetInt("amountOfWood", goodsVariable.amountOfWood);
+            PlayerPrefs.SetInt("amountOfBook", goodsVariable.amountOfBook);
+            PlayerPrefs.SetInt("amountOfCow", goodsVariable.amountOfCow);
+            // 횟수 변수 데이터 파일
+            PlayerPrefs.SetInt("numberOfGetWood", numberVariable.numberOfGetWood);
+        }
+    }
+
+    public void Load()
+    {
+        // 재화 변수 불러오기
+        goodsVariable.amountOfMoney = PlayerPrefs.GetInt("amountOfMoney");
+        goodsVariable.amountOfWood = PlayerPrefs.GetInt("amountOfWood");
+        goodsVariable.amountOfCow = PlayerPrefs.GetInt("amountOfBook");
+        goodsVariable.amountOfCow = PlayerPrefs.GetInt("amountOfCow");
+        // 횟수 변수 불러오기
+        goodsVariable.numberVariable = PlayerPrefs.GetInt("numberOfGetWood");
+    }
 
     public void NextTurn() //다음턴 함수
     {
-        numberOfTurn += 1;
     }
 
     public void GetWood()
     {
-        numberOfMoney -= needOfGetWood;
-        numberOfWood += productOfGetWood;
+        
     }
 }
---
+++ fairytale firm/Assets/Scripts/Variable.cs
@@ -0,0 +1,16 @@
+using System;
+
+[System.Serializable]
+public class GoodsVariable
+{
+    public int amountOfMoney;
+    public int amountOfWood;
+    public int amountOfBook;
+    public int amountOfCow;
+}
+
+[System.Serializable]
+public class NumberVariable
+{
+    public int numberOfGetWood;
+}
