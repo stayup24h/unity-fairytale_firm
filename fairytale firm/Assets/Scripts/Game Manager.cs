6d2873e6908717e57b010abfc00edf7969484040
stayup24h <sani0501we@gmail.com>
2024-04-08 01:34:41 +0900

    fix: wrong classname and variable

--- fairytale firm/Assets/Scripts/Game Manager.cs
+++ fairytale firm/Assets/Scripts/Game Manager.cs
@@ -7,12 +7,12 @@ using UnityEngine.UI;
 public class GameManager : Singleton<GameManager>
 {
     private string hasSavefile = null; // 데이터 파일 존재 여부
-    private int isClearedNE, isClearedTE, isClearedBE, isClearedHE;
+    private int isClearedNE, isClearedTE, isClearedBE, isClearedHE; // 엔딩 클리어 여부
     public GoodsVariable goodsVariable; // 재화 변수
     public NumberVariable numberVariable; // 횟수 변수
     public int productOfGetWood = 3, needOfGetWood = 100;
 
-    public void Save()
+    public void Save() // 데이터 저장
     {
         hasSavefile = PlayerPrefs.GetString("hasSavefile");
         if (hasSavefile != null) // 데이터 파일이 있으면 변경된 값만 저장
@@ -33,7 +33,7 @@ public class GameManager : Singleton<GameManager>
         }
     }
 
-    public void Load()
+    public void Load() // 데이터 불러오기
     {
         // 재화 변수 불러오기
         goodsVariable.amountOfMoney = PlayerPrefs.GetInt("amountOfMoney");
@@ -41,7 +41,7 @@ public class GameManager : Singleton<GameManager>
         goodsVariable.amountOfCow = PlayerPrefs.GetInt("amountOfBook");
         goodsVariable.amountOfCow = PlayerPrefs.GetInt("amountOfCow");
         // 횟수 변수 불러오기
-        goodsVariable.numberVariable = PlayerPrefs.GetInt("numberOfGetWood");
+        numberVariable.numberOfGetWood = PlayerPrefs.GetInt("numberOfGetWood");
     }
 
     public void NextTurn() //다음턴 함수
